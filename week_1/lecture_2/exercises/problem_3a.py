08a31da7b95c9b40bdd45a608769bbab3b43f265
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-05-15 21:43:43 -0300

    L2-P3A: deterministic number

---
+++ week_1/lecture_2/exercises/problem_3a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 15 21:31:42 2016
+
+@author: Lidiane
+"""
+
+def deterministicNumber():
+    '''
+    Deterministically generates and returns an even number between 9 and 21
+    '''        
+    return 10
+
+print deterministicNumber()
